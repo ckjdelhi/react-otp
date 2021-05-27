2001b0b5f8d0d6c9741dddb14f287c403cea6e9c
Chandan Kumar <chandan.krk@gmail.com>
2021-05-27 10:09:31 +0530

    Removed firebase config

--- src/firebase.js
+++ src/firebase.js
@@ -2,12 +2,12 @@ import firebase from 'firebase/app'
 import 'firebase/auth'
 
 var firebaseConfig = {
-    apiKey: "AIzaSyBmk0D4gRRX_2pW_GtQQsw3jekROlT5DVg",
-    authDomain: "otp-app-demo-6770f.firebaseapp.com",
-    projectId: "otp-app-demo-6770f",
-    storageBucket: "otp-app-demo-6770f.appspot.com",
-    messagingSenderId: "32572480615",
-    appId: "1:32572480615:web:41f9d17ee13b08036708c2"
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: ""
   };
   // Initialize Firebase
   firebase.initializeApp(firebaseConfig);
